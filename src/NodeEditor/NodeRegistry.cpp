33d754e5fe8ed2656483bdea37f5206ebde88441
virusbear <fabianfahrenholz718@gmail.com>
2022-01-30 11:46:32 +0100

    Removed old node editor implementation

--- src/NodeEditor/NodeRegistry.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Virusbear on 28.01.2022.
-//
-
-#include "NodeEditor/NodeRegistry.h"
-
-namespace CubiLight {
-    std::map<std::string, NodeFactory> NodeRegistry::m_definitions;
-}
